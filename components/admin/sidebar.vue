6eeec3b990fa89aae2460059a205ea483e5f8072
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-15 14:52:49 +0700

    Update Sidebar Admin

--- components/admin/sidebar.vue
+++ components/admin/sidebar.vue
@@ -1,65 +1,89 @@
 <template>
   <ul class="c-sidebar-nav">
+    <!-- Dashboard -->
+    <li class="c-sidebar-nav-item">
+      <NuxtLink :to="{ name: 'admin-dashboard' }" class="c-sidebar-nav-link">
+        <svg class="c-sidebar-nav-icon" aria-hidden="true">
+          <use xlink:href="/icons/free.svg#cil-speedometer"></use>
+        </svg>
+        Dashboard
+      </NuxtLink>
+    </li>
 
-    <li class="c-sidebar-nav-item"><nuxt-link :to="{name: 'admin-dashboard'}" class="c-sidebar-nav-link" href="index.html">
-        <svg class="c-sidebar-nav-icon">
-          <use xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-speedometer"></use>
-        </svg> Dashboard</nuxt-link></li>
-
+    <!-- Master Data -->
     <li class="c-sidebar-nav-title">MASTER DATA</li>
 
-    <li class="c-sidebar-nav-item"><nuxt-link :to="{name: 'admin-categories'}" class="c-sidebar-nav-link">
-        <svg class="c-sidebar-nav-icon">
-          <use xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-folder"></use>
-        </svg> Categories</nuxt-link>
+    <li class="c-sidebar-nav-item">
+      <NuxtLink :to="{ name: 'admin-categories' }" class="c-sidebar-nav-link">
+        <svg class="c-sidebar-nav-icon" aria-hidden="true">
+          <use xlink:href="/icons/free.svg#cil-folder"></use>
+        </svg>
+        Categories
+      </NuxtLink>
     </li>
 
-    <li class="c-sidebar-nav-item"><nuxt-link :to="{name: 'admin-products'}" class="c-sidebar-nav-link">
-        <svg class="c-sidebar-nav-icon">
-          <use xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-layers"></use>
-        </svg> Products</nuxt-link>
+    <li class="c-sidebar-nav-item">
+      <NuxtLink :to="{ name: 'admin-products' }" class="c-sidebar-nav-link">
+        <svg class="c-sidebar-nav-icon" aria-hidden="true">
+          <use xlink:href="/icons/free.svg#cil-layers"></use>
+        </svg>
+        Products
+      </NuxtLink>
     </li>
 
+    <!-- Orders -->
     <li class="c-sidebar-nav-title">ORDERS</li>
-    
-    <li class="c-sidebar-nav-item"><nuxt-link :to="{name: 'admin-invoices'}" class="c-sidebar-nav-link">
-        <svg class="c-sidebar-nav-icon">
-          <use xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-cart"></use>
-        </svg> Invoices</nuxt-link>
+
+    <li class="c-sidebar-nav-item">
+      <NuxtLink :to="{ name: 'admin-invoices' }" class="c-sidebar-nav-link">
+        <svg class="c-sidebar-nav-icon" aria-hidden="true">
+          <use xlink:href="/icons/free.svg#cil-cart"></use>
+        </svg>
+        Invoices
+      </NuxtLink>
     </li>
 
+    <!-- Others -->
     <li class="c-sidebar-nav-title">OTHERS</li>
 
-    <li class="c-sidebar-nav-item"><nuxt-link :to="{name: 'admin-customers'}" class="c-sidebar-nav-link">
-        <svg class="c-sidebar-nav-icon">
-          <use xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-user"></use>
-        </svg> Customers</nuxt-link>
+    <li class="c-sidebar-nav-item">
+      <NuxtLink :to="{ name: 'admin-customers' }" class="c-sidebar-nav-link">
+        <svg class="c-sidebar-nav-icon" aria-hidden="true">
+          <use xlink:href="/icons/free.svg#cil-user"></use>
+        </svg>
+        Customers
+      </NuxtLink>
     </li>
 
-    <li class="c-sidebar-nav-item"><nuxt-link :to="{name: 'admin-sliders'}" class="c-sidebar-nav-link">
-        <svg class="c-sidebar-nav-icon">
-          <use xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-laptop"></use>
-        </svg> Sliders</nuxt-link>
+    <li class="c-sidebar-nav-item">
+      <NuxtLink :to="{ name: 'admin-sliders' }" class="c-sidebar-nav-link">
+        <svg class="c-sidebar-nav-icon" aria-hidden="true">
+          <use xlink:href="/icons/free.svg#cil-laptop"></use>
+        </svg>
+        Sliders
+      </NuxtLink>
     </li>
 
-    <li class="c-sidebar-nav-item"><nuxt-link :to="{name: 'admin-users'}" class="c-sidebar-nav-link">
-        <svg class="c-sidebar-nav-icon">
-          <use xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-group"></use>
-        </svg> Users</nuxt-link>
+    <li class="c-sidebar-nav-item">
+      <NuxtLink :to="{ name: 'admin-users' }" class="c-sidebar-nav-link">
+        <svg class="c-sidebar-nav-icon" aria-hidden="true">
+          <use xlink:href="/icons/free.svg#cil-group"></use>
+        </svg>
+        Users
+      </NuxtLink>
     </li>
 
+    <!-- Divider -->
     <li class="c-sidebar-nav-divider"></li>
   </ul>
 </template>
 
 <script>
-  export default {
-
-  }
+export default {};
 </script>
 
 <style scoped>
-  a.nuxt-link-active {
-    background: rgba(255,255,255,.05)!important;
-  }
+a.nuxt-link-active {
+  background-color: rgba(255, 255, 255, 0.05) !important;
+}
 </style>
